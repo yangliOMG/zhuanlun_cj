e24e99b12b567bcd9d23216c51795cea6df0e7e7
yangliOMG <562792542@qq.com>
2020-05-27 14:44:42 +0800

    200527修改appit

--- src/pray/shouye/shouye.jsx
+++ src/pray/shouye/shouye.jsx
@@ -26,7 +26,7 @@ class Shouye extends React.Component{
             _user.getUserLogin(isMoblieMode,code).then(res=>this.storageSave(res.data,type))
         }else if(user === ''|| !user.openid){
             if(isMoblieMode){
-                let appid = 'wx0844c3919de8209c',
+                let appid = 'wx6c574f927ea28c68',
                     RedicetURI = window.location.href,
                     uri = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${RedicetURI}&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect`
                 window.location.href = uri;
